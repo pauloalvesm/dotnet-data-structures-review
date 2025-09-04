61c81a10ae3e49c25f453e41180afa004f2e9c7b
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-03 21:58:06 -0300

    feat: apply array exercise one

---
+++ src/01-Arrays/DataStructures.Array01/Program.cs
@@ -0,0 +1,43 @@
+﻿using DataStructures.Helpers.Utilities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Array - Display Even and Odd Numbers **********");
+
+        int arraySize = 10;
+        int[] array = new int[arraySize];
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+
+        Console.WriteLine();
+
+        HelperCollections.ReadArray(array, arraySize);
+        CountEvenAndOdd(array, ref evenNumbers, ref oddNumbers);
+
+        Console.WriteLine("\n-------- Array Elements --------");
+        HelperCollections.PrintArray(array);
+
+        Console.WriteLine("\n-------- Result --------");
+        Console.WriteLine($"Quantity of even numbers: {evenNumbers}");
+        Console.WriteLine($"Quantity of odd numbers: {oddNumbers}");
+
+        Console.ReadKey();
+    }
+
+    private static void CountEvenAndOdd(int[] array, ref int even, ref int odd) 
+    {
+        for (int i = 0; i < array.Length; i++) 
+        {
+            if (array[i] % 2 == 0)
+            {
+                even++;
+            }
+            else 
+            {
+                odd++;
+            }
+        }
+    }
+}
