2cabfe58aabcf5c7246f70d3ea11c74d0cd3606b
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-09 20:05:04 -0300

    feat: apply binary search exercise one

---
+++ src/03-Linear-Binary-Searches/DataStructures.BinarySearch01/Program.cs
@@ -0,0 +1,43 @@
+﻿using DataStructures.Helpers.Utilities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Check Array Sorting **********");
+
+        Console.Write("Enter the array size: ");
+        int arraySize = int.Parse(Console.ReadLine());
+
+        int[] array = new int[arraySize];
+        HelperCollections.ReadArray(array, arraySize);
+
+        bool isSorted = CheckArraySorting(array);
+
+        Console.WriteLine("\n-------- Result --------");
+
+        if (isSorted)
+        {
+            Console.WriteLine("The array is sorted in ascending order.");
+        }
+        else 
+        {
+            Console.WriteLine("The array is NOT sorted in ascending order.");
+        }
+        
+        Console.ReadKey();
+    }
+
+    private static bool CheckArraySorting(int[] array) 
+    {
+        for (int i = 1; i < array.Length; i++) 
+        {
+            if (array[i] < array[i - 1]) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
