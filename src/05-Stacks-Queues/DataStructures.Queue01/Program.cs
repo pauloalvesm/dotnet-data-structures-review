da145d58b030a3f3a11d28975d9cb03af5b1f2e3
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-13 20:06:40 -0300

    feat: apply queue exercise one

---
+++ src/05-Stacks-Queues/DataStructures.Queue01/Program.cs
@@ -0,0 +1,36 @@
+﻿using DataStructures.Queue01.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Queue Data Structure **********");
+
+        QueueModel<int> queue = new QueueModel<int>(5);
+
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+
+        Console.WriteLine($"Element at the front: {queue.Peek()}");
+
+        Console.WriteLine($"Dequeueing: {queue.Dequeue()}");
+        Console.WriteLine($"Dequeueing: {queue.Dequeue()}");
+
+        Console.WriteLine($"Element at the front: {queue.Peek()}");
+
+        queue.Enqueue(40);
+        Console.WriteLine($"Element at the front: {queue.Peek()}");
+
+        while (!queue.IsEmpty())
+        {
+            Console.WriteLine($"Dequeueing: {queue.Dequeue()}");
+        }
+
+        Console.WriteLine("\n-------- Result --------");
+
+        Console.WriteLine($"Queue is empty: {queue.IsEmpty()}");
+
+        Console.ReadKey();
+    }
+}
