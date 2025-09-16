859b35f054aa21331058cae0d9a52a7d3922f59b
pauloalvesm <paulo.alves7351@gmail.com>
2025-09-16 12:07:31 -0300

    feat: define binary tree exercise

---
+++ src/06-Trees/DataStructures.BinaryTree01/Models/BinaryTree.cs
@@ -0,0 +1,72 @@
+﻿namespace DataStructures.BinaryTree01.Models;
+
+public class BinaryTree
+{
+    private int[] tree;
+    private int size;
+
+    public BinaryTree(int capacity)
+    {
+        tree = new int[capacity];
+        size = 0;
+    }
+
+    public void Insert(int inputValue) 
+    {
+        if (size == tree.Length) 
+        {
+            Console.WriteLine("The tree is full");
+            return;
+        }
+
+        tree[size] = inputValue;
+        size++;
+    }
+
+    public void PrintInOrder(int index)
+    {
+        if (!IsIndexValid(index))
+        {
+            return;
+        }
+
+        PrintInOrder(2 * index + 1);
+
+        Console.Write($"{tree[index]}  ");
+
+        PrintInOrder(2 * index + 2);
+    }
+
+    public void PrintPreOrder(int index) 
+    {
+        if (!IsIndexValid(index))
+        {
+            return;
+        }
+
+        Console.Write($"{tree[index]}  ");
+
+        PrintPreOrder(2 * index + 1);
+
+        PrintPreOrder(2 * index + 2);
+    }
+
+    public void PrintPostOrder(int index) 
+    {
+        if (!IsIndexValid(index)) 
+        {
+            return;
+        }
+
+        PrintPostOrder(2 * index + 1);
+
+        PrintPostOrder(2 * index + 2);
+
+        Console.Write($"{tree[index]}  ");
+    }
+
+    private bool IsIndexValid(int index) 
+    {
+        return index < size;
+    }
+}
---
+++ src/06-Trees/DataStructures.BinaryTree01/Program.cs
@@ -0,0 +1,33 @@
+﻿using DataStructures.BinaryTree01.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Data Structure Binary Tree **********");
+
+        BinaryTree binaryTree = new BinaryTree(10);
+
+        binaryTree.Insert(1);
+        binaryTree.Insert(2);
+        binaryTree.Insert(3);
+        binaryTree.Insert(4);
+        binaryTree.Insert(5);
+        binaryTree.Insert(6);
+        binaryTree.Insert(7);
+
+        Console.WriteLine("\nIn-Order Traversal: ");
+        binaryTree.PrintInOrder(0);
+        Console.WriteLine();
+
+        Console.WriteLine("\nPre-Order Traversal: ");
+        binaryTree.PrintPreOrder(0);
+        Console.WriteLine();
+
+        Console.WriteLine("\nPost-Order Traversal: ");
+        binaryTree.PrintPostOrder(0);
+        Console.WriteLine();
+
+        Console.ReadKey();
+    }
+}
